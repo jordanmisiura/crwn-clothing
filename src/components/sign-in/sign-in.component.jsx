89f64e9309f4d44d3465bbcebfc3c9a1e8c7e7cf
jordanmisiura <jordanmisiura@gmail.com>
2020-07-22 21:27:17 -0700

    finished sign in

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
 import './sign-in.styles.scss';
 
@@ -15,13 +15,19 @@ class SignIn extends Component {
         };
     }
 
-    onSubmit = (e)=>{
+    onSubmit = async (e)=>{
         e.preventDefault();
 
-        this.setState({
-            email:'',
-            password:''
-        });
+        const { email, password } = this.state;
+        try {
+            await auth.signInWithEmailAndPassword(email,password);
+            this.setState({
+                email:'',
+                password:''
+            });//clear state after success
+        } catch(error) {
+            console.log("error signing in with email/password: ",error);
+        }
     };
 
     onChange = (e)=>{
